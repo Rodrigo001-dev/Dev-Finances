d6b62c64f1ee4f943590280f579b9af8ee2a1c58
Rodrigo <rodrigorael53@gmail.com>
2021-01-29 04:01:24 -0300

    Colocanco os dados da table do HTML no JavaScript para ficar dinâmico

--- scripts.js
+++ scripts.js
@@ -9,4 +9,51 @@ const Modal = {
       .querySelector('.modal-overlay')
       .classList.remove('active')
   }
-}
+};
+
+const transactions = [
+  { id: 1, description: 'Luz', amount: -50000, date: '23/01/2021' },
+  { id: 2, description: 'Website', amount: 500000, date: '23/01/2021' },
+  { id: 3, description: 'Internet', amount: -20000, date: '23/01/2021' },
+  { id: 4, description: 'App', amount: 200000, date: '23/01/2021' },
+];
+
+const Transaction = {
+  incomes() {
+    // somar entradas
+  },
+  expenses() {
+    // somar saídas
+  },
+  total() {},
+};
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+    //criando a tag tr
+    const tr = document.createElement('tr');
+    // inserindo o HTML dentro do tr
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLTransaction(transaction) {
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="expense">${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+    `;
+
+    return html;
+  },
+};
+
+// para cada item dentro do array eu rodo a função addTransaction e passo 
+// transação do momento
+transactions.forEach((transaction) => {
+  DOM.addTransaction(transaction);
+});
