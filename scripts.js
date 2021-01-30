668a1593b2104c537b7c896254df2856c66501e7
Rodrigo <rodrigorael53@gmail.com>
2021-01-30 16:23:53 -0300

    Pegando os valores de description, amount e date do form e apresentando um alert para o usuario se ele não preencher todos os dados do modal

--- scripts.js
+++ scripts.js
@@ -141,6 +141,38 @@ const Utils = {
   },
 };
 
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,  
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validadeFields() {
+    const { description, amount, date } = Form.getValues();
+
+    if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+  
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validadeFields();
+    } catch (error) {
+      alert(error.message)
+    }
+  },
+};
+
 const App = {
   init() {
     // para cada item dentro do array eu rodo a função addTransaction e passo 
